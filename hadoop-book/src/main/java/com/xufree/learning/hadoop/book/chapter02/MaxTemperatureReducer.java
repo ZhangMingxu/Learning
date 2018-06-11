0da099e39559eabf05409bc95d76a144cd8b26ed
mingx <mingxu_code@hrbust.edu.cn>
2018-06-11 17:33:47 +0800

    实现获取每年最大温度的程序

---
+++ hadoop-book/src/main/java/com/xufree/learning/hadoop/book/chapter02/MaxTemperatureReducer.java
@@ -0,0 +1,25 @@
+package com.xufree.learning.hadoop.book.chapter02;// cc MaxTemperatureReducer Reducer for maximum temperature example
+// vv MaxTemperatureReducer
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class MaxTemperatureReducer
+  extends Reducer<Text, IntWritable, Text, IntWritable> {
+  
+  @Override
+  public void reduce(Text key, Iterable<IntWritable> values,
+                     Context context)
+      throws IOException, InterruptedException {
+    
+    int maxValue = Integer.MIN_VALUE;
+    for (IntWritable value : values) {
+      maxValue = Math.max(maxValue, value.get());
+    }
+    context.write(key, new IntWritable(maxValue));
+  }
+}
+// ^^ MaxTemperatureReducer
