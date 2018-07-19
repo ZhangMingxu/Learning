bff0c2dfd1982c13865f0979d7cb5fbee3f1fe77
zhangmingxu1 <zhangmingxu1@jd.com>
2018-07-19 19:16:17 +0800

    增加线程间通信的Piped测试类 增加ThreadLocal测试类

---
+++ java/src/main/java/com/xufree/learning/java/thread/Piped.java
@@ -0,0 +1,41 @@
+package com.xufree.learning.java.thread;
+
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.PipedWriter;
+
+public class Piped {
+    public static void main(String[] args) throws IOException {
+        PipedReader in = new PipedReader();
+        PipedWriter out = new PipedWriter();
+        out.connect(in);
+        new Thread(new Print(in),"Print Thread").start();
+        int receive;
+        try {
+            while ((receive = System.in.read())!=-1){
+                out.write(receive);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    static class Print implements Runnable{
+        private PipedReader in;
+
+        Print(PipedReader in) {
+            this.in = in;
+        }
+
+        @Override
+        public void run() {
+            int receive;
+            try {
+                while ((receive = in.read()) != -1){
+                    System.out.print((char)receive);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ java/src/main/java/com/xufree/learning/java/thread/Profiler.java
@@ -0,0 +1,32 @@
+package com.xufree.learning.java.thread;
+
+import com.xufree.learning.util.SleepUtil;
+
+import java.util.concurrent.TimeUnit;
+
+public class Profiler {
+    private static final ThreadLocal<Long> TIME_THREADLOCAL = new ThreadLocal<Long>() {
+        @Override
+        protected Long initialValue() {
+            return System.currentTimeMillis();
+        }
+    };
+
+    public static void begin() {
+        TIME_THREADLOCAL.set(now());
+    }
+
+    public static long end() {
+        return System.currentTimeMillis() - TIME_THREADLOCAL.get();
+    }
+
+    private static Long now() {
+        return System.currentTimeMillis();
+    }
+
+    public static void main(String[] args) {
+        Profiler.begin();
+        SleepUtil.second(2);
+        System.out.println("Cost:"+Profiler.end());
+    }
+}
