166a05ec01aacd2546ad2532f10809253407dfaf
zhangmingxu <zhangmingxu1@jd.com>
2019-03-27 11:09:20 +0800

    增加压缩策略配置方式

--- hadoop/src/main/java/com/xufree/learning/hadoop/mapreduce/loganalyse/LogAnalyseMainWithCompression.java
+++ hadoop/src/main/java/com/xufree/learning/hadoop/mapreduce/loganalyse/LogAnalyseMainWithCompression.java
@@ -4,11 +4,13 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.compress.GzipCodec;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 
 import java.io.IOException;
 
@@ -35,6 +37,11 @@ public class LogAnalyseMainWithCompression {
         //TODO 设置输出压缩
         FileOutputFormat.setCompressOutput(job,true);
         FileOutputFormat.setOutputCompressorClass(job, GzipCodec.class);
+        //TODO 设置压缩格式
+        //按照每条记录压缩 默认配置 不推荐
+//        SequenceFileOutputFormat.setOutputCompressionType(job, SequenceFile.CompressionType.RECORD);
+        //针对每一组进行压缩 推荐
+        SequenceFileOutputFormat.setOutputCompressionType(job, SequenceFile.CompressionType.BLOCK);
         job.waitForCompletion(true);
     }
 }
