6b91a9bcd707882e1b96dc21f93bc207f64aff53
zhangmingxu <zhangmingxu1@jd.com>
2019-03-27 10:59:58 +0800

    增加MR任务指定队列名配置 增加MR任务输出压缩配置

--- hadoop/src/main/java/com/xufree/learning/hadoop/mapreduce/loganalyse/LogAnalyseMainWithQueueName.java
+++ hadoop/src/main/java/com/xufree/learning/hadoop/mapreduce/loganalyse/LogAnalyseMainWithQueueName.java
@@ -1,6 +1,5 @@
 package com.xufree.learning.hadoop.mapreduce.loganalyse;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
