6546101aee39489c2019716e4bdfde1f7d341ec4
zhangmingxu <zhangmingxu1@jd.com>
2019-04-23 15:47:12 +0800

    更改配置文件 增加获取某个配置的方法

--- hadoop/src/main/java/com/xufree/learning/hadoop/hdfs/HDFSClient.java
+++ hadoop/src/main/java/com/xufree/learning/hadoop/hdfs/HDFSClient.java
@@ -2,6 +2,8 @@ package com.xufree.learning.hadoop.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -9,12 +11,17 @@ import java.io.IOException;
  * @author zhangmingxu ON 20:44 2019-04-22
  **/
 public class HDFSClient {
+    private static final Logger logger = LoggerFactory.getLogger(HDFSClient.class);
+
     private static Configuration configuration;
     private static FileSystem client;
 
     static {
         configuration = new Configuration();
-        configuration.set("fs.defaultFS", "hdfs://zhangmingxudeMacBook-Pro.local:9000");
+        configuration.addResource("core-site.xml");
+        configuration.addResource("hdfs-site.xml");
+        configuration.addResource("yarn-site.xml");
+        configuration.addResource("mapred-site.xml");
         try {
             client = FileSystem.newInstance(configuration);
         } catch (IOException e) {
@@ -22,7 +29,7 @@ public class HDFSClient {
         }
     }
 
-    public static Configuration getConfiguration(){
+    public static Configuration getConfiguration() {
         return configuration;
     }
 
@@ -31,5 +38,13 @@ public class HDFSClient {
             throw new RuntimeException("实例化HDFS客户端失败");
         }
         return client;
+    }
+
+    public static void printAllConfig() {
+        configuration.forEach(entry -> logger.info("读取配置[{}:{}]", entry.getKey(), entry.getValue()));
+    }
+
+    public String getConfig(String key) {
+        return configuration.get(key);
     }
 }
