f4c9913a4a33946d9a7cb647d84808bc3f5122e5
zhangmingxu <zhangmingxu1@jd.com>
2019-07-05 18:01:37 +0800

    增加Object方法实现

--- java/src/main/java/com/xufree/learning/java/thread/ThreadCommunication.java
+++ java/src/main/java/com/xufree/learning/java/thread/ThreadCommunication.java
@@ -10,14 +10,33 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author zhangmingxu ON 16:53 2019-07-05
  **/
 public class ThreadCommunication {
+    private static volatile int num;
+
     public static void main(String[] args) throws InterruptedException {
+        useObject();
+    }
+
+    private static void useCondition() throws InterruptedException {
         final ReentrantLock lock = new ReentrantLock();
         final Condition flag = lock.newCondition();
         new Thread(() -> {
             lock.lock();
             try {
                 flag.await(); //等待通知
-                System.out.println("await over");
+                System.out.println(Thread.currentThread().getName() + " await over num is " + num);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }).start();
+
+
+        new Thread(() -> {
+            lock.lock();
+            try {
+                flag.await(); //等待通知
+                System.out.println(Thread.currentThread().getName() + " await over num is " + num);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
@@ -30,11 +49,71 @@ public class ThreadCommunication {
         new Thread(() -> {
             lock.lock();
             try {
-                System.out.println("start signal");
-                flag.signal();//发送通知
+                System.out.println("start signal 2");
+                num = 2;
+                flag.signalAll();//发送通知
             } finally {
                 lock.unlock();
             }
         }).start();
+        System.out.println("main sum is " + num);
+
+//        new Thread(() -> {
+//            lock.lock();
+//            try {
+//                System.out.println("start signal 3");
+//                num = 3;
+//                flag.signal();//发送通知
+//            } finally {
+//                lock.unlock();
+//            }
+//        }).start();
+    }
+
+    /**
+     * 等待通知经典范例
+     * syncharonized(object){
+     * while(flag){
+     * object.wait();
+     * }
+     * doSomthing();
+     * }
+     * syncharonized(object){
+     * 改变条件
+     * object.notifyAll();
+     * }
+     */
+    private static void useObject() {
+        class Flag {
+            private boolean flag = false;
+        }
+
+        Flag flag = new Flag();
+        Object lock = new Object();
+        new Thread(() -> {
+            try {
+                synchronized (lock) {
+                    System.out.println("start wait");
+                    while (!flag.flag) {
+                        lock.wait();
+                    }
+                    System.out.println(Thread.currentThread().getName() + " wait over num is " + num);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            synchronized (lock) {
+                System.out.println("notify num 2");
+                num = 2;
+                flag.flag = true;
+                lock.notify();
+
+            }
+        }).start();
+
+
     }
 }
