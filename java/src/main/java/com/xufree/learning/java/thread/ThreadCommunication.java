06f1d410920b389312a937d428b8fb050bac0c95
zhangmingxu <zhangmingxu1@jd.com>
2019-07-08 09:33:14 +0800

    增加线程通信代码

--- java/src/main/java/com/xufree/learning/java/thread/ThreadCommunication.java
+++ java/src/main/java/com/xufree/learning/java/thread/ThreadCommunication.java
@@ -1,119 +1,62 @@
 package com.xufree.learning.java.thread;
 
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.PipedWriter;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * 线程通讯测试
+ * 线程通信
  *
- * @author zhangmingxu ON 16:53 2019-07-05
+ * @author zhangmingxu ON 18:24 2019-07-05
  **/
 public class ThreadCommunication {
-    private static volatile int num;
 
-    public static void main(String[] args) throws InterruptedException {
-        useObject();
-    }
-
-    private static void useCondition() throws InterruptedException {
-        final ReentrantLock lock = new ReentrantLock();
-        final Condition flag = lock.newCondition();
-        new Thread(() -> {
-            lock.lock();
-            try {
-                flag.await(); //等待通知
-                System.out.println(Thread.currentThread().getName() + " await over num is " + num);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                lock.unlock();
-            }
-        }).start();
-
-
-        new Thread(() -> {
-            lock.lock();
-            try {
-                flag.await(); //等待通知
-                System.out.println(Thread.currentThread().getName() + " await over num is " + num);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                lock.unlock();
-            }
-        }).start();
-
-        TimeUnit.SECONDS.sleep(2);
-
-        new Thread(() -> {
-            lock.lock();
-            try {
-                System.out.println("start signal 2");
-                num = 2;
-                flag.signalAll();//发送通知
-            } finally {
-                lock.unlock();
-            }
-        }).start();
-        System.out.println("main sum is " + num);
-
-//        new Thread(() -> {
-//            lock.lock();
-//            try {
-//                System.out.println("start signal 3");
-//                num = 3;
-//                flag.signal();//发送通知
-//            } finally {
-//                lock.unlock();
-//            }
-//        }).start();
-    }
-
-    /**
-     * 等待通知经典范例
-     * syncharonized(object){
-     * while(flag){
-     * object.wait();
-     * }
-     * doSomthing();
-     * }
-     * syncharonized(object){
-     * 改变条件
-     * object.notifyAll();
-     * }
-     */
-    private static void useObject() {
-        class Flag {
-            private boolean flag = false;
-        }
-
-        Flag flag = new Flag();
+    public static void main(String[] args) throws IOException, InterruptedException {
+        PipedReader pipedReader = new PipedReader();
+        PipedWriter pipedWriter = new PipedWriter();
+        pipedReader.connect(pipedWriter);
         Object lock = new Object();
-        new Thread(() -> {
-            try {
+        new Thread(() -> {//读线程
+            while (true) {
                 synchronized (lock) {
-                    System.out.println("start wait");
-                    while (!flag.flag) {
+                    try {
                         lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-                    System.out.println(Thread.currentThread().getName() + " wait over num is " + num);
                 }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                try {
+                    char[] buf = new char[4096];
+                    int len = pipedReader.read(buf);
+                    System.out.println("read thread read :" + new String(buf, 0, len));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }).start();
 
-        new Thread(() -> {
-            synchronized (lock) {
-                System.out.println("notify num 2");
-                num = 2;
-                flag.flag = true;
-                lock.notify();
-
+        TimeUnit.SECONDS.sleep(1);
+        new Thread(() -> { //写线程
+            int i = 1;
+            while (i < 10) {
+                synchronized (lock) {
+                    String message = "message-" + i;
+                    System.out.println("write thread write :" + message);
+                    try {
+                        pipedWriter.write(message);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    i++;
+                    lock.notify();
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         }).start();
-
-
     }
 }
---
+++ java/src/main/java/com/xufree/learning/java/thread/WaitAndNotify.java
@@ -0,0 +1,119 @@
+package com.xufree.learning.java.thread;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 线程等待通知
+ *
+ * @author zhangmingxu ON 16:53 2019-07-05
+ **/
+public class WaitAndNotify {
+    private static volatile int num;
+
+    public static void main(String[] args) throws InterruptedException {
+        useObject();
+    }
+
+    private static void useCondition() throws InterruptedException {
+        final ReentrantLock lock = new ReentrantLock();
+        final Condition flag = lock.newCondition();
+        new Thread(() -> {
+            lock.lock();
+            try {
+                flag.await(); //等待通知
+                System.out.println(Thread.currentThread().getName() + " await over num is " + num);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }).start();
+
+
+        new Thread(() -> {
+            lock.lock();
+            try {
+                flag.await(); //等待通知
+                System.out.println(Thread.currentThread().getName() + " await over num is " + num);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }).start();
+
+        TimeUnit.SECONDS.sleep(2);
+
+        new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println("start signal 2");
+                num = 2;
+                flag.signalAll();//发送通知
+            } finally {
+                lock.unlock();
+            }
+        }).start();
+        System.out.println("main sum is " + num);
+
+//        new Thread(() -> {
+//            lock.lock();
+//            try {
+//                System.out.println("start signal 3");
+//                num = 3;
+//                flag.signal();//发送通知
+//            } finally {
+//                lock.unlock();
+//            }
+//        }).start();
+    }
+
+    /**
+     * 等待通知经典范例
+     * syncharonized(object){
+     * while(flag){
+     * object.wait();
+     * }
+     * doSomthing();
+     * }
+     * syncharonized(object){
+     * 改变条件
+     * object.notifyAll();
+     * }
+     */
+    private static void useObject() {
+        class Flag {
+            private boolean flag = false;
+        }
+
+        Flag flag = new Flag();
+        Object lock = new Object();
+        new Thread(() -> {
+            try {
+                synchronized (lock) {
+                    System.out.println("start wait");
+                    while (!flag.flag) {
+                        lock.wait();
+                    }
+                    System.out.println(Thread.currentThread().getName() + " wait over num is " + num);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            synchronized (lock) {
+                System.out.println("notify num 2");
+                num = 2;
+                flag.flag = true;
+                lock.notify();
+
+            }
+        }).start();
+
+
+    }
+}
