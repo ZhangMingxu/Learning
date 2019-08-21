9c8f435e76c32553b402c630e97987a328c67b14
zhangmingxu <zhangmingxu1@jd.com>
2019-08-21 15:56:44 +0800

    增加获取所有线程和内存gc测试

--- java/src/main/java/com/xufree/learning/java/memory/MemoryInAction.java
+++ java/src/main/java/com/xufree/learning/java/memory/MemoryInAction.java
@@ -1,6 +1,8 @@
 package com.xufree.learning.java.memory;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -9,12 +11,13 @@ import java.util.concurrent.TimeUnit;
  * @author zhangmingxu ON 11:06 2019-08-21
  **/
 public class MemoryInAction {
+    private static final List<BigDecimal> holder = new ArrayList<>();
 
     public static void main(String[] args) {
         while (true) {
             try {
                 useMemory();
-                TimeUnit.MICROSECONDS.sleep(100);
+                TimeUnit.MILLISECONDS.sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -23,7 +26,9 @@ public class MemoryInAction {
 
     private static void useMemory() {
         for (int i = 0; i < 10000; i++) {
+//            holder.add(new BigDecimal("5646545"));
             new BigDecimal("5646545");
         }
+        System.out.println("size: " + holder.size());
     }
 }
